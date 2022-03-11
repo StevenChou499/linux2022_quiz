d5416a21bb55706520f6a0dc7e34d18a77d3b53c
StevenChou499 <stevenchou499@gmail.com>
2022-03-12 01:59:12 +0800

    Add quiz3_3.c file

--- quiz3/quiz3_5.c
+++ quiz3/quiz3_5.c
@@ -1,18 +1,48 @@
-#include <stdint.h>
+#include <limits.h>
 #include <stdio.h>
 
-uint8_t rev8(uint8_t x)
+int divide(int dividend, int divisor) // eg. dividend = 10, divisor = 3
 {
-    x = (x >> 4) | (x << 4); // swap the left and right 4 bits
-    x = ((x & 0xCC) >> 2) | ((x & 0x33) << 2);
-    x = ((x & 0xAA) >> 1) | ((x & 0x55) << 1);
-    return x;
+    int signal = 1;
+    unsigned int dvd = dividend; // dvd = 10
+    if (dividend < 0) { // check if dividend is negative
+        signal *= -1;
+        dvd = ~dvd + 1;
+    }
+
+    unsigned int dvs = divisor; // dvs = 3
+    if (divisor < 0) { // check if divisor is negative
+        signal *= -1;
+        dvs = ~dvs + 1;
+    }
+
+    int shift = 0;
+    while (dvd > (dvs << shift)) // dvd = 10, dvs << shift = 3, shift++, dvd = 10, dvs << shift = 6, shift++
+        shift++; // shift = 2
+
+    unsigned int res = 0;
+    while (dvd >= dvs) {
+        while (dvd < (dvs << shift)) // dvd = 10, dvs << shift = 12, shift--
+            shift--; // shift = 1
+        res |= (unsigned int) 1 << shift;
+        dvd -= res;
+    }
+
+    if (signal == 1 && res >= INT_MAX)
+        return INT_MAX;
+    return res * signal;
 }
 
 int main()
 {
-    uint8_t x = 99;
-    printf("The bits before filpping: %u\n", x);
-    printf("The bits after flipping: %u\n", rev8(x));
+    int a = 133;
+    int b = 3;
+    /*for(int i = 1; i < 100; i++){
+        if(divide(133, i) != (133 / i)){
+            printf("It isn't the same!!\n");
+        }
+        printf("%d\n", i);
+    }*/
+    printf("%d / %d = %d by divide and %d by /\n", a, b, divide(a, b), a / b);
     return 0;
 }
